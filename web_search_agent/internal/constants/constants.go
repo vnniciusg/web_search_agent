427ce5b9d05f46c2fbb8754904e5c14f3c6b986b
vnnicius <vinnicius109@gmail.com>
2026-01-18 13:56:43 -0300

    fix: update constants

--- web_search_agent/internal/constants/constants.go
+++ web_search_agent/internal/constants/constants.go
@@ -4,13 +4,13 @@ const (
 	AppName   = "criticism_research_agent"
 	ModelName = "gemini-3-flash-preview"
 
-	StateResearchFindings  = "research_findings"
-	StateReviewerFeedback  = "reviewer_feedback"
-	StateTechnicalFindings = "technical_findings"
-	StateEthicalFindings   = "ethical_findings"
-	StateSocialFindings    = "social_findings"
-	StateEconomicFindings  = "economic_findings"
-	StateFinalReport       = "final_report"
+	StateResearchFindings   = "research_findings"
+	StateReviewerFeedback   = "reviewer_feedback"
+	StateUserPainPoints     = "user_pain_points_findings"
+	StateCompetitorFindings = "competitor_findings"
+	StateTechnicalFindings  = "technical_findings"
+	StateMarketGapsFindings = "market_gaps_findings"
+	StateFinalReport        = "final_report"
 
 	CompletionPhrase = "RESEARCH_COMPLETE"
 )
